93e3c2bae4898028a66dfa95418c1691db2cb99b
admin <admin@Bcalab3-m08>
2022-03-17 09:41:00 +0530

    added display method

--- GitLab2EX/src/Employee.java
+++ GitLab2EX/src/Employee.java
@@ -1,5 +1,18 @@
 
 public class Employee {
+	String name;
+	Employee(String name)
+	{
+		this.name = name;
+	}
+	void display()
+	{
+		System.out.println("Employee name is " + name);
+	}
+	void print()
+	{
+		
+	}
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
